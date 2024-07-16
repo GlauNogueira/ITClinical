d16bbfa5e14f081c75b70a46e1106245edcc97be
GlauNogueira <nogueiracglaucia@gmail.com>
2024-07-16 23:00:31 +0100

    Criação do primeiro teste

--- it_clinical_java/src/Main.java
+++ it_clinical_java/src/Main.java
@@ -1,5 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+    @Test
+    public void teste_numero_maiusculas(){
+        String nome = "Glaucia";
+        int resultado = numero_letras_maiusculas(nome);
+        Assert.assertEquals(1, resultado);
+    }
+    public int numero_letras_maiusculas(String nome){
+        int maiusculas = 0;
+        return maiusculas;
     }
 }
