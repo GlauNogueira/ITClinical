1d6804f7bc4d6354f2c2cef9a85a52886a33bc88
GlauNogueira <nogueiracglaucia@gmail.com>
2024-07-16 23:13:42 +0100

    Criação de mais 3 testes

--- it_clinical_java/src/Main.java
+++ it_clinical_java/src/Main.java
@@ -2,17 +2,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
     @Test
-    public void teste_numero_maiusculas(){
+    public void teste_numero_maiusculas_nome(){
         String nome = "Glaucia";
         int resultado = numero_letras_maiusculas(nome);
         Assert.assertEquals(1, resultado);
     }
+    @Test
+    public void teste_numero_maiusculas_frase(){
+        String frase = "Uma String de Teste";
+        int resultado = numero_letras_maiusculas(frase);
+        Assert.assertEquals(3, resultado);
+    }
+    @Test
+    public void teste_numero_maiusculas_sem_maiuscula(){
+        String palavra = "gato";
+        int resultado = numero_letras_maiusculas(palavra);
+        Assert.assertEquals(0, resultado);
+    }
+    @Test
+    public void teste_numero_maiusculas_nulo(){
+        String nula = "";
+        int resultado = numero_letras_maiusculas(nula);
+        Assert.assertEquals(0, resultado);
+    }
     public int numero_letras_maiusculas(String nome){
-        int maiusculas = 0;
+        int maiusculas = -1;
         return maiusculas;
     }
 }
