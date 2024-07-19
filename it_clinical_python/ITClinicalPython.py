767d41b6456a5acf6bb6549d969d4f31d3f34846
GlauNogueira <nogueiracglaucia@gmail.com>
2024-07-19 16:43:28 +0100

    Criação primeiro teste

--- it_clinical_python/ITClinicalPython.py
+++ it_clinical_python/ITClinicalPython.py
@@ -5,5 +5,12 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
+class TesteMaiusculas(unittest.TestCase):
+    def teste_numero_maiusculas_nome(self):
+        nome = "Glaucia"
+        resultado = numero_letras_maiusculas(nome)
+        self.assertEqual(1, resultado)
+
+
 if __name__ == '__main__':
     unittest.main()
