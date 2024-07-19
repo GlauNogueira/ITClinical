d02ff4a4e985f4524436ed7e0fa7bf93cf6ccb43
GlauNogueira <nogueiracglaucia@gmail.com>
2024-07-19 17:00:49 +0100

    Criação de mais três testes

--- it_clinical_python/ITClinicalPython.py
+++ it_clinical_python/ITClinicalPython.py
@@ -11,6 +11,20 @@ class TesteMaiusculas(unittest.TestCase):
         resultado = numero_letras_maiusculas(nome)
         self.assertEqual(1, resultado)
 
+    def teste_numero_maiusculas_frase(self):
+        frase = "Uma String de Teste"
+        resultado = numero_letras_maiusculas(frase)
+        self.assertEqual(3, resultado)
+
+    def teste_numero_maiusculas_sem_maiuscula(self):
+        palavra = "gato"
+        resultado = numero_letras_maiusculas(palavra)
+        self.assertEqual(0, resultado)
+
+    def teste_numero_maiusculas_nulo(self):
+        nula = ""
+        resultado = numero_letras_maiusculas(nula)
+        self.assertEqual(0, resultado)
 
 if __name__ == '__main__':
     unittest.main()
